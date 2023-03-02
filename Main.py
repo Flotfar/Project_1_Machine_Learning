f32163e2597f766893878bff1c1e30b9514c6f63
Flotfar <113911826+Flotfar@users.noreply.github.com>
2023-03-02 16:26:52 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,27 +7,36 @@ from scipy.stats import norm
 import seaborn as sns
 from scipy.linalg import svd
 
-# Loading file into str. array 
-# df = np.genfromtxt("LA_Ozone_Data.txt", dtype=int, skip_header=0, encoding=None, delimiter=",")
-# ClassLabels = (df[0,])
-df = pd.read_csv(r"C:\Users\jkdah\git\Project-1---Machine-Learning\LA_Ozone_Data.txt", header=None)
+# Loading file into str. array
+df = df = pd.read_csv("LA_Ozone_Data.txt", header=None)
 
 # Converting the panda dataframe to a numpy array: 
 raw_data = df.values
 data_values = raw_data[1:,:].astype(float)
 class_labels = raw_data[0,:]
 
-
-
-###############################################
-# 3-Data visualization
-###############################################
-
-# Boxplots - Are there any Outliers?
-
 units = ["log(g/m3)", "m", "mph", "%", "F", "m", "Pa/m", "F", "miles", "-"]
 
-def box_plot(data_values, class_labels, units):
+# Categorizing the datapoints in raw_data with quantile placement
+doy = raw_data[1:, 9]
+days = [eval(i) for i in doy]
+days_in_month = [31+28+31, 30+31+30, 31+31+30, 31+30+31]
+month = []
+
+for day in days:
+    for i, days_in_this_month in enumerate(days_in_month):
+        if day <= days_in_this_month:
+            month.append(i+1)
+            break
+        else:
+            day -= days_in_this_month
+
+##########################################################
+# Attribute Boxplot
+##########################################################
+
+
+def box_plot(values, labels, units):
 
     rows = 3
     columns = 3
@@ -40,38 +49,38 @@ def box_plot(data_values, class_labels, units):
     for r in range(rows):
         for c in range(columns):
             current_boxplot = axes[r, c]
-            current_boxplot.set_title(f'{class_labels[count]} [{units[count]}]')
-            current_boxplot.boxplot(data_values[1:,count])
+            current_boxplot.set_title(f'{labels[count]} [{units[count]}]')
+            current_boxplot.boxplot(values[1:,count])
 
             count += 1
 
     plt.show()
 
-#box_plot(data_values, class_labels,units)
 
-# Boxplots - Are there any Outliers?
+#### Detecting outliers from the attributes ########
+def outliers(values, labels):
 
-def outliers(class_labels, data_values):
-
-    for k in range(len(data_values[0,:])):
-        q1, q3= np.percentile(data_values[:,k],[25,75])
+    for k in range(len(values[0,:])):
+        q1, q3= np.percentile(values[:,k],[25,75])
         iqr = q3 - q1
         lower_bound = q1 -(1.5 * iqr) 
         upper_bound = q3 +(1.5 * iqr)
 
         local_outliers = 0
-        for i in range(len(data_values[:,0])):
-            if data_values[i,k] <= upper_bound and data_values[i,k] >= lower_bound:
+        for i in range(len(values[:,0])):
+            if values[i,k] <= upper_bound and values[i,k] >= lower_bound:
                 pass
             else:
                 local_outliers += 1
         
-        print(f"Attribute {class_labels[k]} contains {local_outliers} outliers.")
-
-#outliers(class_labels, data_values)
+        print(f"Attribute {labels[k]} contains {local_outliers} outliers.")
 
 
-def normal_distribution(class_labels, data_values):
+##########################################################
+# Attribute Normal destribution
+##########################################################
+
+def normal_distribution(values, labels):
     
     rows = 3
     columns = 3
@@ -86,12 +95,12 @@ def normal_distribution(class_labels, data_values):
             #Making the 3x3 array for histogram plots
             current_histogram = axes[r, c]
             #Setting the title for each subplot
-            current_histogram.set_title(f'{class_labels[count]}')
+            current_histogram.set_title(f'{labels[count]}')
             #plotting each subplot as a histograd using the probability density function(pdf)
-            current_histogram.hist(data_values[:,count], bins=bin_number, density=True, alpha=1, color='r')
+            current_histogram.hist(values[:,count], bins=bin_number, density=True, alpha=1, color='r')
             #determining the mean and std for each attributes, and using this to plot a normal distrubution
-            mean, std = norm.fit(data_values[:,count])
-            normal_data = np.linspace(min(data_values[:,count]), max(data_values[:,count]), 80)
+            mean, std = norm.fit(values[:,count])
+            normal_data = np.linspace(min(values[:,count]), max(values[:,count]), 80)
             current_histogram.plot(normal_data, norm.pdf(normal_data, mean, std), 'b', linewidth=2)
 
             #adding a value to count, to go to next subbplot
@@ -99,156 +108,106 @@ def normal_distribution(class_labels, data_values):
 
     plt.show()
 
-#normal_distribution(class_labels, data_values)
 
+##########################################################
+# Attribute corelation heatmap
+##########################################################
 
-
-def correlation_heatmap(data_values, class_labels):
+def correlation_heatmap(values, labels):
     # checking correlation using heatmap
-    data_frame = pd.DataFrame(data_values[:,:9], columns = class_labels[:9])
+    data_frame = pd.DataFrame(values[:,:9], columns = labels[:9])
     correlation_mat = data_frame.corr()
     mask = np.triu(np.ones_like(correlation_mat, dtype=bool))
     sns.heatmap(correlation_mat, mask = mask, annot = True)
     plt.show()
 
-#correlation_heatmap(data_values, class_labels)
 
 
-def pca_cummulative(data_values):
+##########################################################
+# Principal Component Analysis (PCA)
+##########################################################
 
-    N = len(data_values[:,0])
+#Subtrackting doy from the dataset to reduce noise:
+X = np.delete(data_values, np.s_[9], 1)
 
-    columns_to_delete = [2, 5, 6, 8]
-    plot_titles = ['Variance explained by principal components (all attributes)',
-                   'Variance explained by principal components (selected attributes)']
+# Normalizing the data:
+N = len(data_values[:,0])
+Y = (X - X.mean(axis=0)*np.ones((N,1))) / X.std(axis=0)*np.ones((N,1))
 
-    pc_coefficients = [5,2]
+# Running the Single Value Decompositioning (SVD)
+U, S, V = svd(Y,full_matrices=False)
 
-    #Removing doy from data_values
-    X_all = np.delete(data_values, -1, 1)
-    #Removing un-selected data from X_all
-    X_selected = np.delete(X_all, columns_to_delete, 1)
+# Compute variance explained by principal components
+rho = (S*S) / (S*S).sum()
 
-    #For loop over the to datasets
-    for i, X in enumerate([X_all, X_selected]):
-        
-        #Normalizing the data
-        Y = (X - X.mean(axis=0)*np.ones((N,1))) / X.std(axis=0)*np.ones((N,1))
 
-        #Using exercise 2_1_3 to plot varience explained
-        # PCA by computing SVD of Y 
-        U,S,V = svd(Y,full_matrices=False)
 
-        # Compute variance explained by principal components
-        rho = (S*S) / (S*S).sum() 
+##### Visualizing the coherance of the PC's ########## 
+def pca_cummulative(input):
 
-        #printing the pc_coefficients for all (5) and selected attributes (2)
-        print(V[:pc_coefficients[i]])
+    #Defining data-set quantiles
+    Q1 = 0.25
+    Q2 = 0.50
+    Q3 = 0.75
 
-        #Defining the threshold
-        threshold = 0.9
-
-        # Plot variance explained in two subplots, for selected and all data
-        plt.subplot(1, 2, i + 1)
-        plt.plot(range(1,len(rho)+1),rho,'x-')
-        plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
-        plt.plot([1,len(rho)],[threshold, threshold],'k--')
-        plt.title(plot_titles[i]);
-        plt.xlabel('Principal component');
-        plt.ylabel('Variance explained');
-        plt.legend(['Individual','Cumulative','Threshold'])
-        plt.grid()
-
+    # Plot variance explained in two subplots, for selected and all data
+    plt.figure()
+    plt.plot(range(1,len(input)+1),input,'x-')
+    plt.plot(range(1,len(input)+1),np.cumsum(input),'o-')
+    plt.plot([0,len(input)],[Q1, Q1],':', color='r', linewidth=1)
+    plt.plot([0,len(input)],[Q2, Q2],':', color='r', linewidth=1)
+    plt.plot([0,len(input)],[Q3, Q3],':', color='r', linewidth=1)
+    plt.title('Variance explained by principal components (all attributes)');
+    plt.xlabel('Principal component');
+    plt.ylabel('Variance explained');
+    plt.legend(['Individual','Cumulative','Quantiles'])
+    plt.grid()
 
     #plotting
     plt.show()
 
-pca_cummulative(data_values)
 
 
-"""
-def pca_bar_chart(data_values, class_labels):
+##### plotting the PCA aginst the 3 most influencial PC's ##########
+def pca_visualization(input):
 
-    N = len(data_values[:,0])
+    fig1 = plt.figure(figsize=(10,8), facecolor='w')
+    ax = fig1.add_subplot(projection='3d')
 
-    columns_to_delete = [2, 5, 6, 8]
-    plot_titles = ['PC coefficients (all attributes)',
-                   'PC coefficients (selected attributes)']
+    for i in range(input.shape[0]):
+        x = V[0,:] @ input[i,1:].T
+        y = V[1,:] @ input[i,1:].T
+        z = V[2,:] @ input[i,1:].T
 
-    #Removing doy from data_values
-    X_all = np.delete(data_values, -1, 1)
-    label_all = np.delete(class_labels, -1, 0)
-    #Removing un-selected data from X_all
-    X_selected = np.delete(X_all, columns_to_delete, 1)
-    label_selected = np.delete(class_labels, columns_to_delete, 0)
-    pc_coefficients = [5,2]
-
-    
-
-    #For loop over the to datasets
-    for i, X in enumerate([X_all, X_selected]):
-        
-        #Normalizing the data
-        Y = (X - X.mean(axis=0)*np.ones((N,1))) / X.std(axis=0)*np.ones((N,1))
-
-        #Using exercise 2_1_3 to plot varience explained
-        # PCA by computing SVD of Y 
-        U,S,V = svd(Y,full_matrices=False)
-        V_current = V[:pc_coefficients[i]]
-        # Plot variance explained in two subplots, for selected and all data
-        plt.subplot(1, 2, i + 1)
-        
-        # create data
-        
-        # plot data in grouped manner of bar type
-        if i == 0:
-
-            x = np.arange(pc_coefficients[i])
-            width = 0.2
-
-            plt.bar(x-0.4, V_current[0], width, color='red')
-            plt.bar(x-0.2, V_current[1], width, color='cyan')
-            plt.bar(x, V_current[2], width, color='orange')
-            plt.bar(x+0.2, V_current[3], width, color='green')
-            plt.bar(x+0.4, V_current[4], width, color='magenta')
-            plt.xticks(x, label_all)
-            plt.xlabel("Attributes")
-            plt.ylabel("PC coefficients")
-            plt.legend(["PC1", "PC2","PC3","PC4","PC5"])
-
-    #plotting
-    plt.show()
-
-pca_bar_chart(data_values, class_labels)
-"""
-correlation_heatmap(data_values, class_labels)
-
-
-
-###############################################
-# PCA1 & PCA2 plotting 
-###############################################
-
-# creating month 'moy' column into the array for categorization
-doy = raw_data[1:, 9]
-days = [eval(i) for i in doy]
-days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-month = ["moy"]
-
-for day in days:
-    for i, days_in_this_month in enumerate(days_in_month):
-        if day <= days_in_this_month:
-            month.append(i+1)
-            break
+        if month[i] == 1:
+            ax.scatter(x,y,z, marker='o', color='gold', s=20)
+        elif month[i] == 2:
+            ax.scatter(x,y,z, marker='o', color='dodgerblue', s=20)
+        elif month[i] == 3:
+            ax.scatter(x,y,z, marker='o', color='limegreen', s=20)
         else:
-            day -= days_in_this_month
+            ax.scatter(x,y,z, marker='o', color='darkorange', s=20)
 
-with_months = np.column_stack((raw_data, month))
+    ax.set_xlabel('\nPC1', fontsize = 15, linespacing=1)
+    ax.set_ylabel('\nPC2', fontsize = 15, linespacing=2)
+    ax.set_zlabel('\nPC3 ', fontsize = 15, linespacing=2)
+    ax.view_init(25,-50)
+    # creating dummy plot for legend applyance: 
+    proxy1 = plt.Line2D([0],[0], linestyle="none", color='gold', marker = 'o')
+    proxy2 = plt.Line2D([0],[0], linestyle="none", color='dodgerblue', marker = 'o')
+    proxy3 = plt.Line2D([0],[0], linestyle="none", color='limegreen', marker = 'o')
+    proxy4 = plt.Line2D([0],[0], linestyle="none", color='darkorange', marker = 'o')
+    ax.legend([proxy1, proxy2, proxy3, proxy4], ['Q1', 'Q2', 'Q3', 'Q4' ], numpoints = 1)
 
-# extracting non correlating attributes and related column:
-columns = [2, 5, 6, 8, 9]
-data = np.delete(with_months, np.s_[2, 5, 6, 8, 9], 1)
+    plt.show()
 
 
+##### printing the plots ##########
+box_plot(data_values, class_labels, units)
+outliers(data_values, class_labels)
+normal_distribution(data_values, class_labels)
+correlation_heatmap(data_values, class_labels)
+pca_cummulative(rho)
+pca_visualization(data_values)
 
 
